9a209bfbd0046132458861682124d435bac55438
Cícero Duarte <ciceroduarte1@gmail.com>
2017-02-05 18:26:27 -0200

    Adding JSON keys/values validation

---
+++ lib/json_validator.rb
@@ -0,0 +1,67 @@
+require "rubygems"
+require 'json'
+require './lib/file_to_check.rb'
+require './lib/json_fetcher.rb'
+
+class JSONValidator
+  
+  def self.validate_with_config(config)
+    
+    config['files'].each do |file|
+      fileToCheck = FileToCheck.new(file)
+      
+      fileContent = ""      
+
+      if !fileToCheck.path.nil?
+        fileContent = JSONFetcher.json_from_path(fileToCheck.path)
+      elsif !fileToCheck.remotePath.nil?
+        fileContent = JSONFetcher.json_from_url(fileToCheck.remotePath)
+      else
+        puts "[ERROR] path or remote-path not found"
+        return
+      end      
+      
+      jsonValidator = JSONValidator.new()
+      jsonValidator.validate_JSON_with_keys(fileToCheck.keys, fileContent)
+      
+    end
+  end
+  
+    def validate_JSON_with_keys(jsonKeys, json)
+      jsonKeys.keys.each do |key|
+          value = value_for_key(key, json)
+          configValue = jsonKeys[key]
+          verify_value(configValue, value, key)
+      end
+  end
+  
+  def value_for_key(key, json)
+      return value_for_key_with_split_character(key, json, '.')
+  end
+  
+  def is_numeric?(obj) 
+   obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+  end
+  
+  def value_for_key_with_split_character(key, json, splitCharacter)
+      value = json
+      key.split(splitCharacter).each do |item|
+          if !value.nil? && !item.empty?
+            value = is_numeric?(item) ? value[item.to_i] : value[item]
+          end
+      end
+      return value
+  end
+  
+  def verify_value(configValue, value, key)
+      formatter = "%{first} | Key: %{second} | Expected value: %{third} | Value: %{fourth}"
+      result = "[Error]    "
+      if value.nil?
+          result = "[Not found]"
+      elsif value == configValue
+          result = "[Success]  "
+      end
+      puts formatter % {first: result, second: key, third: configValue, fourth:value}
+  end
+  
+end
