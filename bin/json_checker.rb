06de85af63a0522fbde70d2ebafe5eeb9077b275
Cícero Duarte <ciceroduarte1@gmail.com>
2017-02-06 20:39:19 -0200

    Verify if json-config.json file exists

--- bin/json_checker.rb
+++ bin/json_checker.rb
@@ -2,4 +2,9 @@ require './lib/json_fetcher.rb'
 require './lib/json_validator.rb'
 
 jsonConfig = JSONFetcher.json_from_path('json-config.json')
-JSONValidator.validate_with_config(jsonConfig)
+
+if !jsonConfig.nil?
+  JSONValidator.validate_with_config(jsonConfig)
+else 
+  puts "[ERROR] json-config.json not found"
+end
--- lib/json_fetcher.rb
+++ lib/json_fetcher.rb
@@ -5,9 +5,12 @@ require 'net/http'
 class JSONFetcher
   
   def self.json_from_path(path)
-    jsonFile = open(path)
-    jsonContent = jsonFile.read
-    return JSON.parse(jsonContent)  
+    if File.file?(path)
+      jsonFile = open(path)  
+      jsonContent = jsonFile.read
+      return JSON.parse(jsonContent)
+    end
+    return nil
   end
   
   def self.json_from_url(url)
