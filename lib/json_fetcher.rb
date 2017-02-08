a5bb0cca692ffe9c9fb29bfc5a35aeb7353f3e11
Cícero Duarte <ciceroduarte1@gmail.com>
2017-02-07 22:45:46 -0200

    Connection and JSON error

--- lib/json_fetcher.rb
+++ lib/json_fetcher.rb
@@ -8,15 +8,30 @@ class JSONFetcher
     if File.file?(path)
       jsonFile = open(path)
       jsonContent = jsonFile.read
-      return JSON.parse(jsonContent)
+      return JSONFetcher.json_from_content(jsonContent)
     end
     return nil
   end
   
   def self.json_from_url(url)
     uri = URI(url)
-    response = Net::HTTP.get(uri)
-    return JSON.parse(response)
+    response = Net::HTTP.get_response(uri)
+    case response
+      when Net::HTTPSuccess then
+      return JSONFetcher.json_from_content(response.body)
+    else
+      puts"[ERROR] Connection error"
+    end
+    return nil
+  end
+  
+  def self.json_from_content(content)
+    begin
+      return JSON.parse(content)
+    rescue JSON::ParserError => e
+      puts "[ERROR] Invalid json"
+    end
+    return nil
   end
   
 end
