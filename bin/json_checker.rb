d8f75a3ffd563d08c9d8b5e688347745cc3f4e1a
Cícero Duarte <ciceroduarte1@gmail.com>
2017-02-11 22:01:50 -0200

    Removed json_checker from bin

--- bin/json_checker.rb
+++
@@ -1,8 +0,0 @@
-require './lib/json_fetcher.rb'
-require './lib/json_validator.rb'
-
-jsonConfig = JSONFetcher.json_from_path('json-config.json')
-
-unless jsonConfig.nil?
-  JSONValidator.validate_with_config(jsonConfig)
-end
