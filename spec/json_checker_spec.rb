999dd41f4059c3cdf540177a1bab27352d4649e9
Cícero Duarte <ciceroduarte1@gmail.com>
2017-02-13 21:44:13 -0200

    Adding json_checker tests

--- spec/json_checker_spec.rb
+++ spec/json_checker_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe JsonChecker do
   it "has a version number" do
     expect(JsonChecker::VERSION).not_to be nil
   end
+
+  it "Checker is running" do
+  	checker = JsonChecker::Checker.new
+  	checker.run()
+  end
+  
 end
